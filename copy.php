c88bc71d1f2709ae1506ba9423a83fe04e563f1c
shahid <shahidurrahman732@gmail.com>
2018-04-18 03:19:02 +0600

    copy.php file added

---
+++ copy.php
@@ -0,0 +1,68 @@
+<?php require_once'head.php'?>
+<body>
+
+<div id="main">
+
+	<div class="top">
+		<div class="top_left">
+			<div class="logo">
+				<img src="logo.gif">
+			</div>
+          </div>
+			<div class="top_right">
+				<h1>ONLINE EXAM</h1>
+				<p>chol School e jabi </p>
+			</div>
+		
+      </div>
+		
+		<?php require_once'menu.php'?>
+		<br>
+		<br>
+		<?php require_once'slide.php'?>
+	
+	<div class="content">
+		<table style="width:100%; color:green;background:beige;">
+	<tbody><tr>
+			<td rowspan="5"> <h1> <i> <b> This is a paragraph</b> </i> </h1>
+			<p> <i> <b> bangladesh is a beautifull country
+			<br>bangladesh is a beautifull country
+			bangladesh is a beautifull country
+			<br>bangladesh is a beautifull country
+			<br>bangladesh is a beautifull country
+			bangladesh is a beautifull country
+			<br>bangladesh is a beautifull country<br>
+			bangladesh is a beautifull country
+			bangladesh is a beautifull country
+			<br>bangladesh is a beautifull country
+			bangladesh is a beautifull country
+			<br>bangladesh is a beautifull country
+			bangladesh is a beautifull country
+			<br>bangladesh is a beautifull country
+			bangladesh is a beautifull country.<a href="#">Read more.....</a></b></i><b> </b>
+			</p>
+			</td>
+			<td> <h1> <i> <u> <b>Site Title</b></u></i><u><b></b></u><b></b><h1><b><u><i> </i> </u> </b> </h1></h1></td>
+	</tr>
+	<tr>
+			<td colspan="3"> <a href="registration.php"> <i><b> <button type="button"><h2> Free sign up<h2></button></b></i></a> </td>
+	</tr>
+	<tr>
+			<td colspan="3"> <a href="#"><b><i> <button type="button"><h2> Login<h2></button> </i></b></a> </td>
+	</tr>
+	
+</tbody></table>
+	 </div>
+	
+
+<?php require_once'footer.php'?>
+
+
+</div>
+	
+	
+
+</div>
+
+</body>
+</html>
